ec35bbeba7491710936f84138ea26df49bdfc777
Arseny Ptitsyn <senyabird@yandex.ru>
2020-02-28 14:11:35 +0300

    Первая программа на Java

---
+++ Первая программа на Java/Example1.java
@@ -0,0 +1,11 @@
+/*
+	Это пример простой программы на Java.
+	Присвойте файлу с исходным кодом имя Example1.java
+*/
+class Example1 {
+	// Выполнение любой программы Java начинается
+	// с вызова метода main()
+	public static void main(String args[]) {
+		System.out.println("Java rules the Internet!");
+	}
+}
