d061f02c6d0ac8adb6c6d0a892a336913f2933a2
Arseny Ptitsyn <senyabird@yandex.ru>
2020-03-15 19:57:34 +0300

    Добавил новый проект DistanceToLightening

---
+++ Глава 2. Знакомство с типами данных и операторами/5. Расстояине до места вспышки молнии/DistanceToLightening.java
@@ -0,0 +1,12 @@
+/*
+	Программа вычисляет расстояние до молнии по времени, за которое гром доходит до наблюдателя (7.2 c).
+*/
+class DistanceToLightening{
+	public static void main(String args[]){
+		double dist;
+
+		dist = 1100 * 7.2;
+
+		System.out.println("Расстояние до места вспышки молнии составляет " + dist + " футов");
+	}
+}
